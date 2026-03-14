e4f8bb655f8dd5bb4492e27dbd6148f3f09c449c
apnadost-maker <patelkinjal1212@gmail.com>
2026-03-14 11:23:26 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8287212718:AAEPfAZ5Nbar3TTBM6uTz4fyh7HYqLjP8XE")
+API_ID = int(environ.get("API_ID", "27433400"))
+API_HASH = environ.get("API_HASH", "1a286620de5ffe0a7d9b57e604293555")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "7809571505"))
+OWNER = int(environ.get("OWNER", "6201066540"))
 CREDIT = environ.get("CREDIT", "Engineers Babu")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '7809571505').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '6201066540').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7809571505').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '6201066540').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
